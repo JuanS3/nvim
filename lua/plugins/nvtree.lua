b1c6bfe34bf1b9df87c345479d51cf504d85cf6f
Sebastián Martínez <sebastian.martinez.serna@gmail.com>
2024-05-13 21:59:22 -0500

    feat: :art: better toggle

--- lua/plugins/nvtree.lua
+++ lua/plugins/nvtree.lua
@@ -1,23 +1,71 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-vim.opt.termguicolors = true
 
-require("nvim-tree").setup({
-  sort_by = "case_sensitive",
+require('nvim-tree').setup({
+  sort_by = 'case_sensitive',
   view = {
     adaptive_size = true,
+    number = true,
+    relativenumber = true,
+    signcolumn = 'yes',
   },
   renderer = {
     group_empty = true,
+    highlight_git = true,
+    special_files = {
+      'README.md',
+      'Makefile',
+      'package.json',
+      'LICENSE',
+      '.gitignore',
+      'requirements.txt',
+    },
+    icons = {
+      show = {
+        file = true,
+        folder = true,
+        folder_arrow = true,
+        git = true,
+        modified = true,
+        diagnostics = true,
+        bookmarks = true,
+      },
+      glyphs = {
+        default = '',
+        symlink = '',
+        folder = {
+          arrow_closed = '',
+          arrow_open = '↯',
+          default = '',
+          open = '',
+          empty = '',
+          empty_open = '',
+          symlink = '',
+          symlink_open = '',
+        },
+        git = {
+          unstaged = '󰦒',
+          staged = '󰸞',
+          unmerged = '',
+          renamed = '➜',
+          untracked = '',
+          deleted = '󰗨',
+          ignored = '',
+        }
+      }
+    }
   },
   filters = {
     dotfiles = true,
   },
+  git = {
+    enable = true,
+  },
 })
 
-vim.keymap.set("n", "<C-b>", ":NvimTreeToggle<CR>")
-vim.keymap.set("n", "<C-b>f", ":NvimTreeFindFile<CR>")
-vim.keymap.set("n", "<C-b>r", ":NvimTreeRefresh<CR>")
+vim.keymap.set('n', '<C-b>', ':NvimTreeToggle<CR>')
+vim.keymap.set('n', '<C-b>f', ':NvimTreeFindFile<CR>')
+vim.keymap.set('n', '<C-b>r', ':NvimTreeRefresh<CR>')
 
 -- close nvim-tree on last buffer
 
