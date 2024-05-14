d24f3e167f699a30ab7ef56f6e45660aadf08a4d
Sebastián Martínez <sebastian.martinez.serna@gmail.com>
2024-05-13 22:23:37 -0500

    feat: :sparkles: add map for git stash, lsp document and symbols

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -14,10 +14,15 @@ vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
 vim.keymap.set('n', '<leader>gcm', builtin.git_commits, {})
 vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
 vim.keymap.set('n', '<leader>gl', builtin.git_bcommits, {})
+vim.keymap.set('n', '<leader>gt', builtin.git_stash, {})
 
 -- LSP
 vim.keymap.set('n', '<leader>ld', builtin.lsp_definitions, {})
 vim.keymap.set('n', '<leader>lr', builtin.lsp_references, {})
 vim.keymap.set('n', '<leader>li', builtin.lsp_implementations, {})
 vim.keymap.set('n', '<leader>lh', builtin.lsp_type_definitions, {})
+vim.keymap.set('n', '<leader>ls', builtin.lsp_document_symbols, {})
 
+-- Misc
+-- Symbols
+vim.keymap.set('n', '<leader>ms', ':Telescope symbols<cr>', {})
