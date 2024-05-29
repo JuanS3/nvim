0d0e97a91b522df66889838f6d51fa51312c4c3d
Sebastián Martínez <sebastian.martinez.serna@gmail.com>
2024-05-29 13:04:31 -0500

    style: :art: just improve format style

--- lua/options.lua
+++ lua/options.lua
@@ -1,5 +1,5 @@
-vim.opt.encoding = "utf-8" -- set encoding
-vim.opt.nu = true -- enable line numbers
+vim.opt.encoding = "utf-8"    -- set encoding
+vim.opt.nu = true             -- enable line numbers
 vim.opt.relativenumber = true -- relative line numbers
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -8,6 +8,9 @@ vim.opt.expandtab = true -- convert tabs to spaces
 vim.opt.smartindent = true -- smart indentation
 vim.opt.clipboard = "unnamedplus" -- use system clipboard
 
+vim.opt.cursorline = true -- highlight current line
+vim.opt.cursorcolumn = true -- highlight current column
+
 vim.wo.wrap = false -- do not wrap lines
 vim.opt.autoindent = true -- auto indentation
 vim.opt.list = true -- show tab characters and trailing whitespace
@@ -15,27 +18,27 @@ vim.opt.formatoptions:remove("t") -- no auto-intent of line breaks, keep line wr
 vim.opt.listchars = "tab:»\\ ,extends:›,precedes:‹,nbsp:·,trail:·" -- show tab characters and trailing whitespace
 
 
-vim.opt.ignorecase = true -- ignore case when searching
-vim.opt.smartcase = true -- unless capital letter in search
+vim.opt.ignorecase = true                              -- ignore case when searching
+vim.opt.smartcase = true                               -- unless capital letter in search
 
-vim.opt.swapfile = false -- do not use a swap file for the buffer
-vim.opt.backup = false -- do not keep a backup file
+vim.opt.swapfile = false                               -- do not use a swap file for the buffer
+vim.opt.backup = false                                 -- do not keep a backup file
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir" -- set directory where undo files are stored
-vim.opt.undofile = true -- save undo history to a file
+vim.opt.undofile = true                                -- save undo history to a file
 
-vim.opt.hlsearch = false -- do not highlight all matches on previous search pattern
-vim.opt.incsearch = true -- incrementally highlight searches as you type
+vim.opt.hlsearch = true                                -- do not highlight all matches on previous search pattern
+vim.opt.incsearch = true                               -- incrementally highlight searches as you type
 
-vim.opt.termguicolors = true -- enable true color support
+vim.opt.termguicolors = true                           -- enable true color support
 
-vim.opt.scrolloff = 8 -- minimum number of lines to keep above and below the cursor
-vim.opt.sidescrolloff = 8 --minimum number of columns to keep above and below the cursor
-vim.opt.signcolumn = "yes" -- always show the sign column, to avoid text shifting when signs are displayed
-vim.opt.isfname:append("@-@") -- include '@' in the set of characters considered part of a file name
+vim.opt.scrolloff = 8                                  -- minimum number of lines to keep above and below the cursor
+vim.opt.sidescrolloff = 8                              --minimum number of columns to keep above and below the cursor
+vim.opt.signcolumn = "yes"                             -- always show the sign column, to avoid text shifting when signs are displayed
+vim.opt.isfname:append("@-@")                          -- include '@' in the set of characters considered part of a file name
 
-vim.opt.updatetime = 50 -- Time in milliseconds to wait before triggering the plugin events after a change
+vim.opt.updatetime = 50                                -- Time in milliseconds to wait before triggering the plugin events after a change
 
-vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
   pattern = "*.py",
   callback = function()
     vim.opt.textwidth = 120
@@ -43,8 +46,8 @@ vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
   end
 }) -- python formatting
 
-vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
-  pattern = {"*.js", "*.html", "*.css", "*.lua"},
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = { "*.js", "*.html", "*.css", "*.lua" },
   callback = function()
     vim.opt.tabstop = 2
     vim.opt.softtabstop = 2
@@ -53,29 +56,29 @@ vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
 }) -- javascript formatting
 
 vim.api.nvim_create_autocmd("BufReadPost", {
-    pattern = "*",
-    callback = function()
-      if vim.fn.line("'\"") > 0 and vim.fn.line("'\"") <= vim.fn.line("$") then
-        vim.cmd("normal! g`\"")
-      end
+  pattern = "*",
+  callback = function()
+    if vim.fn.line("'\"") > 0 and vim.fn.line("'\"") <= vim.fn.line("$") then
+      vim.cmd("normal! g`\"")
     end
+  end
 }) -- return to last edit position when opening files
 
 
 local HighlightYank = vim.api.nvim_create_augroup('HighlightYank', {})
 vim.api.nvim_create_autocmd('TextYankPost', {
-    group = HighlightYank,
-    pattern = '*',
-    callback = function()
-        vim.highlight.on_yank({
-            higroup = 'IncSearch',
-            timeout = 40,
-        })
-    end,
+  group = HighlightYank,
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = 'IncSearch',
+      timeout = 40,
+    })
+  end,
 }) -- highlight yanked text using the 'IncSearch' highlight group for 40ms
 
 local CleanOnSave = vim.api.nvim_create_augroup('CleanOnSave', {})
-vim.api.nvim_create_autocmd({"BufWritePre"}, {
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   group = CleanOnSave,
   pattern = "*",
   command = [[%s/\s\+$//e]],
@@ -87,4 +90,3 @@ vim.api.nvim_create_autocmd("bufWritePost", {
   pattern = "*.py",
   command = "silent !black %",
 })
-
