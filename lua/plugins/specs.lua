4cf858871037f8b3138c485001b5a27b1e95d617
Sebastián Martínez <sebastian.martinez.serna@gmail.com>
2024-06-04 17:45:57 -0500

    feat: :sparkles: update specs config

--- lua/plugins/specs.lua
+++ lua/plugins/specs.lua
@@ -1,19 +1,17 @@
-require('specs').setup({
-  show_jumps = true,
-  min_jump = 30,
+local specs = require('specs')
+
+
+specs.setup({
   popup = {
-    delay_ms = 0, -- delay before popup displays
-    inc_ms = 10, -- time increments used for fade/resize effects
-    blend = 10, -- starting blend, between 0-100 (fully transparent), see :h winblend
-    width = 10,
-    winhl = "PMenu",
-    fader = require('specs').linear_fader,
-    resizer = require('specs').shrink_resizer
-  },
-  ignore_filetypes = {},
-  ignore_buftypes = {
-    nofile = true,
+    inc_ms = 10,  -- time increments used for fade/resize effects
+    width = 120,
+    winhl = 'Search',
+    fader = specs.pulse_fader,
+    resizer = specs.shrink_resizer,
   },
 })
 
-vim.api.nvim_set_keymap('n', '<leader>v', ':lua require("specs").show_specs({width = 120, winhl = "Search", delay_ms = 10, inc_ms = 5})<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>v',
+  ':lua require("specs").show_specs()<CR>',
+  { noremap = true, silent = true }
+)
