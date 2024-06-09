6969d131c0e69b86f8a1427984b7e20fa15647ae
Sebastián Martínez <sebastian.martinez.serna@gmail.com>
2024-06-08 23:54:12 -0500

    feat: add specs for n and N

--- lua/plugins/specs.lua
+++ lua/plugins/specs.lua
@@ -1,17 +1,23 @@
 local specs = require('specs')
 
-
 specs.setup({
   popup = {
-    inc_ms = 10,  -- time increments used for fade/resize effects
+    inc_ms = 10, -- time increments used for fade/resize effects
     width = 120,
     winhl = 'Search',
-    fader = specs.pulse_fader,
-    resizer = specs.shrink_resizer,
   },
 })
 
 vim.api.nvim_set_keymap('n', '<leader>v',
   ':lua require("specs").show_specs()<CR>',
+  { noremap = true, silent = true }
+)
+
+vim.api.nvim_set_keymap('n', 'n',
+  'n:lua require("specs").show_specs()<CR>',
+  { noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap('n', 'N',
+  'N:lua require("specs").show_specs()<CR>',
   { noremap = true, silent = true }
 )
